115d3e11b85527f3c5ed2dae7e32055122769cc6
DimaMalah <dmitryaronov77@gmail.com>
2022-10-14 01:42:09 +0300

    router fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,16 +17,16 @@ export default function Header (){
 			<div className="header__container">
 				<ul className="nav justify-content-end">
 				<li className="nav-item">
-					<a className="nav-link active" onClick={()=>navigate("/PET_Currency_Exchange/Active")} aria-current="page" href="#">Active</a>
+					<a className="nav-link active" onClick={()=>navigate("/PET_currency_exchange_react/Active")} aria-current="page" href="#">Active</a>
 				</li>
 				<li className="nav-item">
-					<a className="nav-link"  onClick={()=>navigate("/PET_Currency_Exchange/Link")} href="#">Link</a>
+					<a className="nav-link"  onClick={()=>navigate("/PET_currency_exchange_react/Link")} href="#">Link</a>
 				</li>
 				<li className="nav-item">
-					<a className="nav-link" onClick={()=>navigate("/PET_Currency_Exchange/Link_2")} href="#">Link_2</a>
+					<a className="nav-link" onClick={()=>navigate("/PET_currency_exchange_react/Link_2")} href="#">Link_2</a>
 				</li>
 				<li className="nav-item">
-					<a className="nav-link disabled" onClick={()=>navigate("/PET_Currency_Exchange/Disabled")} href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+					<a className="nav-link disabled"  href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
 				</li>
 				</ul>
 				<div className="header__icons">
